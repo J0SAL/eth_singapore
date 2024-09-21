c07632d00a2d23572228d577df0fc05f70f3fdec
J0SAL <joyalmeida321@gmail.com>
2024-09-21 11:48:30 +0530

    mapper changed in contracts

---
+++ frontend/src/components/home/TestContract.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TestContract() {
+    return (
+        <div>TestContract</div>
+    )
+}
+
+export default TestContract
--- frontend/src/utils/Routes.jsx
+++ frontend/src/utils/Routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/home/Home";
 import Profile from "../components/profile/Profile";
+import TestContract from "../components/home/TestContract";
 
 function CustomRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/test" element={<TestContract />} />
     </Routes>
   );
 }
